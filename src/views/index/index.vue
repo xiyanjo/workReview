4f1302ea4b14304695742fc37e229a6cc1c1e86e
xiyanjo <1447656442@qq.com>
2019-02-20 16:32:51 +0800

    '使用less'

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <div style="background:blue;">总入口页面</div>
-    <div style="display:flex;flex-direction:col;">
-      <div style="display:flex;flex-direction:col;">
+    <div class="flexDirectionRow" >
+      <div  class="flexDirectionRow">
 
         <left-menu v-if="isMenu" style="background:#eee;"></left-menu>
-        
-        <div style="width:150px">
+
+        <div style="width:50px">
           <div v-show="!isMenu" @click="isMenu=true">显示</div>
           <div v-show="isMenu" @click="isMenu=false">隐藏</div>
         </div>
