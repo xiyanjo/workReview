c8f4e274e9061278511adcddd28c7328a72f6c23
xiyanjo <1447656442@qq.com>
2019-02-26 10:48:36 +0800

    'vuexStateMution'

--- src/components/aboutVuex/primer.vue
+++ src/components/aboutVuex/primer.vue
@@ -4,13 +4,23 @@
     <FooterBar v-if="isShow" />
   </div> -->
   <div>
-      平日sfsdfsfds
+    <header>vuex使用</header>
+    <br>
+    <div>
+      <span>{{'count-----'+count}}</span> 
+      <button @click="increaseCount">增加</button>
+      <button @click="reduceCount">减少</button>
+    </div>
+  
   </div>
 </template>
 
 <script>
 // import FooterBar from '@/components/common/FooterBar'
 // import config from './config/index'
+
+import { mapState } from 'vuex'
+
 export default {
   name: 'App',
   components:{
@@ -21,9 +31,15 @@ export default {
     }
   },
   computed:{
-     isShow(){
-       return this.$store.getters.isShow;
-     }
+     ...mapState(['count'])
+  },
+  methods: {
+    reduceCount(){
+      this.$store.commit('reduceCount',2)
+    },
+    increaseCount(){
+      his.$store.commit('increaseCount')
+    }
   },
   watch:{
       $route(to,from){ //跳转组件页面后，监听路由参数中对应的当前页面以及上一个页面
@@ -31,18 +47,15 @@ export default {
         if(to.name=='book'||to.name=='my'){ // to.name来获取当前所显示的页面，从而控制该显示或隐藏footerBar组件
            this.$store.dispatch('showFooter') // 利用派发全局state.showFooter的值来控制        }else{
            this.$store.dispatch('hideFooter')
-        }
-      }
-  },
-  mounted(){
-    console.log('primer.vue')
-  }
-}
-</script>
         }else{
            this.$store.dispatch('hideFooter')
         }
       }
-  }
+  },
+  mounted(){
+
+  },
+
 }
 </script>
+  
