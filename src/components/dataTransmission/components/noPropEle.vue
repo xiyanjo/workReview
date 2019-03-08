6356504f38e15b22c859acfc0babaad512f82cf9
xiyanjo <1447656442@qq.com>
2019-03-08 14:15:43 +0800

    'yuansu‘

--- src/components/dataTransmission/components/noPropEle.vue
+++ src/components/dataTransmission/components/noPropEle.vue
@@ -1,11 +1,14 @@
 <template>
  <div>
-
+    <div>
+      <div  v-bind="$attrs">非prop属性在元素上使用</div>
+    </div>
  </div>
 </template>
 
 <script>
  export default {
+    inheritAttrs: false,
    data () {
      return {
 
